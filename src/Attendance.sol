c8166248bfa3afbb1435b147381461e54e438434
Conner Swenberg <conner.swenberg@coinbase.com>
2025-01-28 15:38:02 -0800

    Add isActive function

--- src/Attendance.sol
+++ src/Attendance.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/// @notice Create time-bound sessions others can mark themselves as attending.
 contract Attendance {
     struct Session {
         uint48 start;
@@ -19,10 +20,18 @@ contract Attendance {
     error SessionDoesNotExist(uint256 sessionId, uint256 totalSessions);
     error HasAttendedSession(uint256 sessionId, address sender);
 
+    /// @notice Get the total number of sessions created.
     function totalSessions() external view returns (uint256) {
         return sessions.length;
     }
 
+    /// @notice Check if a session is currently active.
+    function isActive(uint256 sessionId) external view returns (bool) {
+        Session memory session = sessions[sessionId];
+        return block.timestamp >= session.start && block.timestamp < session.end;
+    }
+
+    /// @notice Create a new session.
     function createSession(uint48 start, uint48 end) external returns (uint256 sessionId) {
         if (start >= end) revert InvalidStartEnd(start, end);
 
@@ -31,11 +40,13 @@ contract Attendance {
         emit SessionCreated(sessionId, msg.sender, start, end);
     }
 
+    /// @notice Attend an active session.
+    /// @dev Sessions can only be attended only once per address.
     function attendSession(uint256 sessionId) external {
         if (sessionId > sessions.length - 1) revert SessionDoesNotExist(sessionId, sessions.length);
 
         Session storage session = sessions[sessionId];
-        if (block.timestamp < session.start || block.timestamp > session.end) {
+        if (block.timestamp < session.start || block.timestamp >= session.end) {
             revert InvalidStartEnd(session.start, session.end);
         }
         if (hasAttended[sessionId][msg.sender]) revert HasAttendedSession(sessionId, msg.sender);
